a341bbe651580a7727bbe8f9fa83e3c35d77e594
MSI-PC <MSI-PC@DESKTOP-QT7DPF8.telus>
2017-12-24 17:14:40 -0800

    new

--- src/testBackGit/hello1.java
+++ src/testBackGit/hello1.java
@@ -5,7 +5,7 @@ public class hello1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Hello144121");
+	System.out.println("Hello14554121");
 	
 	}
 
